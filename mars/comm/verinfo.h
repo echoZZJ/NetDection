340bf8103b32fb0b8351d2a84e274e3bb75ff1bb
echoZZJ <zhangzijieecho@gmail.com>
2022-03-16 17:29:30 +0800

    update andrpoid static library arch type

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "1193d370"
+#define MARS_REVISION "b32d0b3b"
 #define MARS_PATH "dev"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-08-05 11:12:28"
+#define MARS_BUILD_TIME "2022-03-16 17:21:44"
 #define MARS_TAG ""
 
 #endif
